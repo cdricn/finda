ff53e11bc38fb9f39d2bc52c589ec5cae32bbe1d
cdricn <cdricncdricn@gmail.com>
2025-11-09 15:15:06 +0800

    Test run of scraper

--- tests/grab_gamejams.spec.ts
+++ tests/grab_gamejams.spec.ts
@@ -1,3 +1,4 @@
+// run this for ui: npx playwright test --ui
 import { test, expect } from '@playwright/test';
 
 test('grab gamejams', {tag: '@scrape'}, async ({ page }) => {
@@ -6,10 +7,23 @@ test('grab gamejams', {tag: '@scrape'}, async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/itch.io/);
 
-  let i = 0;
-  while (i < 5) {
-    
-    
-    i++;
-  }
+  const elementLocator = page.locator('css=.jam');
+  const allEntries = await elementLocator.allInnerTexts();
+  const selectedEntries = allEntries.slice(0, 5);
+
+  console.log(selectedEntries)
+
+  const jsonData = JSON.stringify(selectedEntries, null, 2)
+
+  const fs = require('fs');
+  fs.writeFileSync('output.json', jsonData);
 });
+
+
+
+
+/*await page
+    .getByRole('listitem')
+    .filter({ has: page.getByRole('heading', { name: 'Product 2' }) })
+    .getByRole('button', { name: 'Add to cart' })
+    .click(); */
