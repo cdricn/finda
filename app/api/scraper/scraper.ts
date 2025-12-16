9ede4e69ef6d0312130143d339623dd596005726
cdricn <cdricncdricn@gmail.com>
2025-12-16 22:14:36 +0800

    Added error handling for scraper

--- app/api/scraper/scraper.ts
+++ app/api/scraper/scraper.ts
@@ -1,9 +1,14 @@
 async function FetchJams() {
-  const response = await fetch('http://localhost:8000/jams', { method: "GET"});
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch('http://localhost:8000/jams', { method: "GET"});
+    const data = await response.json();
+    return data;
+  }
+  catch (e) {
+    console.log("Data fetch failed. ", e)
+  }
 }
-export default FetchJams;
 
-//
+export default FetchJams;
 //https://itch-jamcommunity-api.vercel.app/jams
+
