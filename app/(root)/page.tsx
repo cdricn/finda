cd3cee7139a818d39b0e28024f1a4f41a4dd7c39
cdricn <cdricncdricn@gmail.com>
2025-12-20 22:14:52 +0800

    Added header in main

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,27 +3,25 @@
 import FilterSection from '../filter/filterSection'
 import styles from './page.module.css';
 import FetchJams from '../api/scraper/scraper';
+import Header from '../components/header';
 // import { MdViewList, MdViewQuilt  } from "react-icons/md";
 
 export default async function Forums() {
   const gamejams = await FetchJams();
 
-
   return (
-    <>
-      <main className={styles['main-container']}>
-        <section className={styles['filter-section']}>
-          <FilterSection gamejams={gamejams} />
+    <main className={styles['main-container']}>
+      <section className={styles['content-section']}>
+        <header className={styles['header-section']}>
+          <Header gamejams={gamejams} />
+        </header>
+        <section className={styles['posts-section']}>
+          
         </section>
-        <section className={styles['content-section']}>
-          <section className={styles['header-section']}>
-            
-          </section>
-          <section className={styles['posts-section']}>
-            
-          </section>
-        </section>
-      </main>
-    </>
+      </section>
+      <section className={styles['filter-section']}>
+        <FilterSection />
+      </section>
+    </main>
   )
 }
