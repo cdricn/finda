58da675a9ed3a91e59e8b55361982d0f2b89c497
cdricn <cdricncdricn@gmail.com>
2026-01-15 21:15:14 +0800

    Added data fetching for community posts

--- app/api/fetch/dataFetcher.ts
+++ app/api/fetch/dataFetcher.ts
@@ -10,3 +10,19 @@ export async function FetchJams() {
     return error;
   }
 }
+
+export async function FetchPosts(url:string) {
+  if(!url) return;
+
+  const newUrl = url.slice(25);
+
+  try {
+    const response = await fetch(`http://localhost:8000/posts/${newUrl}`, { method: "GET"});
+    const data = await response.json();
+    
+    return data;
+  }
+  catch (error) {
+    return error;
+  }
+}
--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -6,6 +6,16 @@ export interface GameJam {
   host: string;
 }
 
+export interface ForumPosts {
+  title: string,
+  url: string,
+  description: string,
+  replies: number,
+  datePosted: string,
+  author: string
+  tag: string;
+}
+
 export interface TagType {
   [tag:string] : boolean;
 }
