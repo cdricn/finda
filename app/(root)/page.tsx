62d75dab8d939c20f91938242573a30cae022529
cdricn <cdricncdricn@gmail.com>
2026-01-15 21:14:39 +0800

    Added Posts in main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,25 @@
 'use client'
 
 import styles from './page.module.css';
-import Posts from '../components/posts';
+import Posts from '../posts/posts';
 import FilterLayout from '../filter/filterLayout';
+import { useState } from 'react';
 
 export default function Forums() {
+  const [url, setUrl] = useState('');
 
   function handleJamChange(url:string) {
-    console.log('url in main',url)
+    setUrl(url);
   }
 
   return (
     <>
       <main className={styles['main-container']}>
-        <section>
+        <section className={styles['content-section']}>
           <FilterLayout handleJamChange={handleJamChange}/>
         </section>
         <section className={styles['content-section']}>
-          <Posts link={''}/>
+          <Posts link={url}/>
         </section>
       </main>
     </>
