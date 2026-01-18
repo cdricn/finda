ed61349a1fbca986c99dfc5c40e5c2793a4a9f68
cdricn <cdricncdricn@gmail.com>
2026-01-18 15:35:58 +0800

    Changed layout when loading

--- app/components/jamTitle.tsx
+++ app/components/jamTitle.tsx
@@ -4,16 +4,20 @@ import { GameJam } from '@/app/lib/interface';
 export default function JamTitle({jamDetails}:{jamDetails : GameJam}) {
   return (
     <div className={styles['jam-info-container']}>
-      <h1 className={styles['jam-title']}>
-        {jamDetails.url == ''? 
-          <p>Select a gamejam!</p> :
-          <a href={jamDetails.url} target='_'>{jamDetails.title}</a>
-        }
-      </h1>
-      <div className={styles['jam-info']}>
-        <span>Hosted by: {jamDetails.host ? jamDetails.host : '--'}</span>
-        <span>Members: {jamDetails.members}</span>
-      </div>
+      {jamDetails.url == ''?
+        <h1 className={styles['jam-title']}>
+          <p>Select a gamejam!</p>
+        </h1> :
+        <>
+          <h1 className={styles['jam-title']}>
+            <a href={jamDetails.url} target='_'>{jamDetails.title}</a>
+          </h1>
+          <div className={styles['jam-info']}>
+            <span>Hosted by: {jamDetails.host}</span>
+            <span>Members: {jamDetails.members}</span>
+          </div>
+        </>
+      }
     </div>
   )
 }
