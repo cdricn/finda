3a24cced28319329cb8e1c460088f3f6b502a749
cdricn <cdricncdricn@gmail.com>
2026-01-20 17:58:32 +0800

    Added comment for magic number

--- app/api/fetch/dataFetcher.ts
+++ app/api/fetch/dataFetcher.ts
@@ -14,7 +14,13 @@ export async function FetchJams() {
 export async function FetchPosts(url:string) {
   if(!url) return;
 
-  const newUrl = url.slice(25);
+  // dumbass band aid fix
+  // url string is passed with a /api/ in front, so we're just removing that
+  // we're already slicing in filterJams.tsx, but that's just to make the link pretty
+  // idk how I can exclude /api/, I'm p sure I missed something. Get back to it later
+  // console.log(url) // Check if url changed 
+  const excessString = 5;
+  const newUrl = url.slice(excessString); 
 
   try {
     const response = await fetch(`http://localhost:8000/posts/${newUrl}`, { method: "GET"});
