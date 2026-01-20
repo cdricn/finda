796f42a949ea0385f9b9a983fa64e4453ebb7457
cdricn <cdricncdricn@gmail.com>
2026-01-20 18:00:48 +0800

    Added param routing & updated skeleton components

--- app/posts/posts.tsx
+++ app/posts/posts.tsx
@@ -1,16 +1,22 @@
-// import { MdViewList, MdViewQuilt  } from "react-icons/md";
-import styles from './posts.module.css';
+'use client';
+
+import styles from './posts.module.css'
 import PostCard from './postCard';
+import PageMessage from '../skeleton/pageMessage';
 import PostCardSkeleton from '../skeleton/postCardSkeleton';
-import PlaceholderNoResult from '../skeleton/placeholderNoResult';
-import PlaceholderErrorFetch from '../skeleton/placeholderErrorFetch';
-import PlaceholderError404 from '../skeleton/placeholderError404';
 import useSWR from 'swr'
 import { FetchPosts } from '../api/fetch/dataFetcher';
 import { ForumPosts } from '../lib/interface';
+import { useParams, useSearchParams } from 'next/navigation';
 
-export default function Posts({link}:{link: string}) {
-  const { data, isLoading, error } = useSWR<ForumPosts[]>(`/api/${link}`, FetchPosts, {
+export default function Posts() {
+  const params = useParams();
+  const searchParams = useSearchParams(); //for filter
+
+  // console.log(params.id)
+
+  const link = 'swakjam-2026' // for test
+  const { data, isLoading, error } = useSWR<ForumPosts[]>(`/api/${params.id}`, FetchPosts, {
     onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
       if (error.status === 404) return;
       if (retryCount >= 10) return;
@@ -18,10 +24,8 @@ export default function Posts({link}:{link: string}) {
     }
   });
 
-  //there's a better way to check this bruh ong
-  if(data) if(Object.keys(data).length <= 0) return <PlaceholderErrorFetch />;
   if(isLoading) return <PostCardSkeleton />; 
-  if(error) return <PlaceholderError404 />;
+  if(error) return <PageMessage mainText='Error 404.' subText='Something went wrong.'/>;
 
   return (
     <>
@@ -31,7 +35,10 @@ export default function Posts({link}:{link: string}) {
             return <PostCard entry={item} key={item.title+index}/>
           })}
         </ul> :
-        <PlaceholderNoResult />
+        <PageMessage 
+          mainText='No results found.'
+          subText='Try looking in another gamejam.'
+        />
       }
     </>
   )
