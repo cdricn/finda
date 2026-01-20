72f629cbbc534acbfbd0b4cb6e49dbb98d08e162
cdricn <cdricncdricn@gmail.com>
2026-01-20 17:58:53 +0800

    Added github logo

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,14 @@
 import styles from './header.module.css'
+import { DiGithubBadge } from "react-icons/di";
 
 export default function Header() {
   return (
     <header className={styles['header-container']}>
       <span className={styles['header-title']}>findateam</span>
-      <div className={styles['github-icon']}></div>
+      <a href='https://github.com/cdricn/finda' target='_' 
+        className={styles['github-icon']}>
+          <DiGithubBadge />
+      </a>
     </header>
   )
 }
