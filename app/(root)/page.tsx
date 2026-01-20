30d107112cc0792d87626b77cfd5dd1aed94d04e
cdricn <cdricncdricn@gmail.com>
2026-01-21 00:23:41 +0800

    Changed styling for root

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,21 @@
 import styles from './page.module.css';
-import FilterLayout from "../filter/filterLayout";
+import FilterJams from '../filter/filterJams';
 import PageMessage from '../skeleton/pageMessage';
 
 export default function Main() {
 
   return (
-    <>
-      <main className={styles['main-container']}>
-        <FilterLayout />
+    <main className={styles['main-container']}>
+      <section className={styles['title-section']}>
+        <h1>Find a team!</h1>
+        <FilterJams />
+      </section>
+      <section className={styles['content-section']}>
         <PageMessage 
           mainText='Nothing here.' 
           subText='Select a game jam to discover groups or members.'
         />
-      </main>
-    </>
+      </section>
+    </main>
   )
 }
--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -1,12 +1,10 @@
 import Header from "@/app/components/header"
-import FilterLayout from "@/app/filter/filterLayout"
 
 export default function Layout({children} : Readonly<{children: React.ReactNode}>) {
 
   return (
     <>
       <Header />
-      <FilterLayout />
       {children}
     </>
   )
