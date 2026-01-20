e88d08d47488dd98c04c06e9a29d721163e5a446
cdricn <cdricncdricn@gmail.com>
2026-01-20 17:57:54 +0800

    Removed prop drilling from page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,17 @@
-'use client'
-
 import styles from './page.module.css';
-import Posts from '../posts/posts';
-import FilterLayout from '../filter/filterLayout';
-import { useState } from 'react';
+import FilterLayout from "../filter/filterLayout";
+import PageMessage from '../skeleton/pageMessage';
 
-export default function Forums() {
-  const [url, setUrl] = useState('');
-
-  function handleJamChange(url:string) {
-    setUrl(url);
-  }
+export default function Main() {
 
   return (
     <>
       <main className={styles['main-container']}>
-        <section className={styles['content-section']}>
-          <FilterLayout handleJamChange={handleJamChange}/>
-        </section>
-        <section className={styles['content-section']}>
-          <Posts link={url}/>
-        </section>
+        <FilterLayout />
+        <PageMessage 
+          mainText='Nothing here.' 
+          subText='Select a game jam to discover groups or members.'
+        />
       </main>
     </>
   )
