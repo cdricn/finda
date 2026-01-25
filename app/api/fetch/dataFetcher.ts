30c8033bd5e8bf861d885421b931db508618f6ba
cdricn <cdricncdricn@gmail.com>
2026-01-25 16:57:03 +0800

    Moved tags adding to api

--- app/api/fetch/dataFetcher.ts
+++ app/api/fetch/dataFetcher.ts
@@ -26,65 +26,10 @@ export async function FetchPosts(url:string) {
   const excessString = 5;
   const newUrl = url.slice(excessString);
   const link = `${apiLink}/gamejam/posts/${newUrl}`;
-  
-  // can't check wrong spelling; maybe address it later
-  const definedTags : TagType = {
-    programmer: "developer", 
-    developer: "developer",
-    dev: "developer",
-    coder: "developer",
-    sound: "music",
-    audio: "music",
-    musician: "music",
-    music: "music",
-    composer: "music", 
-    sfx: "music",
-    artist: "artist", 
-    voice: "voice actor", 
-    actor: "voice actor",
-    va: "voice actor",
-    narrative: "writer",
-    writer: "writer",
-    playtester: "playtester"
-  };
 
   try {
     const response = await fetch(link, { method: "GET"});
     const data = await response.json();
-
-    //loop through data to get each entry
-    for(const i in data) {
-      //split array based on space to get words
-      const titleArray = data[i].title.split(" ");
-      //add an empty object to data to store tags as key/value
-      data[i].tags = {};
-      //loop through new array
-      for (const word in titleArray) {
-        let title = titleArray[word].toLowerCase();
-        //if word is plural, remove s
-        if (title[title.length-1] === 's') title=title.slice(0,title.length-1);
-        
-        if (definedTags.hasOwnProperty(title)) {
-          //remove special chars
-          title = title.replace(/[^a-zA-Z0-9]/g, '');
-          data[i].tags[definedTags[title]] = definedTags[title];
-        }
-        //if the word has a slash, we split again; ex: "musician/composer"
-        else if (titleArray[word].includes("/")) {
-          //split first, then remove remaining special characters
-          let [firstWord, secondWord] = title.split("/");
-          firstWord = firstWord.replace(/[^a-zA-Z0-9]/g, '');
-          secondWord = secondWord.replace(/[^a-zA-Z0-9]/g, '');
-          
-          if (definedTags.hasOwnProperty(firstWord)) {
-            data[i].tags[definedTags[firstWord]] = definedTags[firstWord];
-          }
-          if (definedTags.hasOwnProperty(secondWord)) {
-            data[i].tags[definedTags[secondWord]] = definedTags[secondWord];
-          }
-        }
-      }
-    }
     return data;
   }
   catch (error) {
