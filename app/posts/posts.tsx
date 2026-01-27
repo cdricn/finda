5f4cef97291312e23691b77f84e435f45ccbb13c
cdricn <cdricncdricn@gmail.com>
2026-01-27 21:09:46 +0800

    Changed error message

--- app/posts/posts.tsx
+++ app/posts/posts.tsx
@@ -25,7 +25,7 @@ export default function Posts() {
   });
   
   if(isLoading) return <LoadingPosts />; 
-  if(error) return <PageMessage mainText='Error 404.' subText='Something went wrong.'/>;
+  if(error) return <PageMessage mainText='Error 404.' subText="This game jam doesn't have a community forums yet."/>;
   
   return (
     <>
