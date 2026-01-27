01f4b1fb6fe73fc2d063d4bb9232f5e270bccee0
cdricn <cdricncdricn@gmail.com>
2026-01-27 17:36:35 +0800

    Fixed styling

--- app/skeleton/disabledSelect.tsx
+++ app/skeleton/disabledSelect.tsx
@@ -2,8 +2,8 @@ import styles from './disabledSelect.module.css';
 
 export default function DisabledSelect() {
   return (
-    <select className={styles['select-disabled']} defaultValue="default" >
-      <option value="default" disabled>--Choose a gamejam--</option>
+    <select className={styles['select-disabled']} defaultValue="default">
+      <option value="default" disabled>Loading gamejams...</option>
     </select>
   )
 }
