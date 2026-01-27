1dd435268621542bf39bc260fb1ed2a74d09ab2a
cdricn <cdricncdricn@gmail.com>
2026-01-27 21:42:39 +0800

    Fixed linking

--- app/posts/postFooter.tsx
+++ app/posts/postFooter.tsx
@@ -3,12 +3,13 @@ import { ParamValue } from 'next/dist/server/request/params';
 
 export default function PostFooter({text, link}:{text:string, link:ParamValue}) {
 
-  const communityLink = link ? link.toString() : '';
+  const communityLink = link ? 
+    'https://itch.io/jam/'+ link.toString() + '/community': '';
 
   return (
     <div className={styles['footer-container']}>
       <span className={styles['footer-text']}>{text}</span>
-      <a href={communityLink} className={styles['button-container']}>
+      <a href={communityLink} className={styles['button-container']} target='_'>
         <span className={styles['button-text']}>Make a post</span>
       </a>
     </div>
