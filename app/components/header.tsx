dc3ae83caaf80475e158216c1bb0d77b0738b42b
cdricn <cdricncdricn@gmail.com>
2026-02-04 18:34:08 +0800

    Updated header link

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import styles from './header.module.css'
 import { DiGithubBadge } from "react-icons/di";
 
 export default function Header() {
   return (
     <header className={styles['header-container']}>
-      <span className={styles['header-title']}>findateam</span>
+      <Link href={'https://findateam.vercel.app/'} className={styles['header-title']}>
+        findateam
+      </Link>
       <a href='https://github.com/cdricn/finda' target='_' 
         className={styles['github-icon']}>
           <DiGithubBadge />
