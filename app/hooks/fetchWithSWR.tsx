a9327ce6b6208e7d478f9bcd46a082b7763752cf
cdricn <cdricncdricn@gmail.com>
2026-03-13 18:33:36 +0800

    Added custom hook to fetch SWR data

---
+++ app/hooks/fetchWithSWR.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import useSWR from 'swr';
+import { FetchPosts } from '../api/fetch/dataFetcher';
+import { FetchResult } from '../lib/interface';
+
+export function fetchWithSWR<T>(params:string): FetchResult<T> {
+
+  const { data, isLoading, error } = useSWR(
+    `/api/posts/${params}`, FetchPosts, {
+      revalidateOnFocus: false,
+      errorRetryCount: 5,
+      onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+        if (error.status === 404) return;
+        if (retryCount >= 5) return;
+        setTimeout(() => revalidate({ retryCount }), 5000);
+      }
+  });
+
+  return {data, isLoading, error};
+}
