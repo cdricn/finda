e2e64b20c9e3299d99fa1cdd602b58ef2c8240b9
cdricn <cdricncdricn@gmail.com>
2026-03-13 18:14:48 +0800

    Modified landing page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,22 +5,19 @@ export default function Main() {
 
   return (
     <main className={styles['main-container']}>
-      <section className={styles['title-section']}>
-        <h1>Find a team!</h1>
-        <FilterJams />
-      </section>
-      <section className={styles['content-section']}>
-        <p>
-          <br/>
-          findateam collects all posts looking for teams and members from popular 
-          itch.io game jam community forums to help you with your team formation.
-          <br/><br/>
-          No more sifting through theme questions, unrelated topics, and spam. 
-          Filter the posts based on the role you want and connect!<br/><br/>
+      <section className={styles['section']}>
+        <h1 className={styles['title-section']}>
+          Find a team!
+        </h1>
+        <p className={styles['content-section']}>
+          Find people easier by filtering posts from itch.io game jams.
+        </p>
+        <div className={styles['search-container']}>
+          <FilterJams />
           <span className={styles['note']}>
             *Game jams with less than 300 participants are not included.
           </span>
-        </p>
+        </div>
       </section>
     </main>
   )
