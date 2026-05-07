81ea57f3fb34122ab2867bffb036378076595d0c
cdricn <cdricncdricn@gmail.com>
2026-05-08 01:37:12 +0800

    Combined cards to posts

--- app/[id]/pagePosts/posts.tsx
+++ app/[id]/pagePosts/posts.tsx
@@ -3,7 +3,6 @@
 import styles from './posts.module.css';
 import { useParams, useSearchParams } from 'next/navigation';
 import { ForumPosts } from '../../lib/interface';
-import Card from './components/card';
 import Message from './components/message';
 
 export default function Posts({data}:{data:ForumPosts[]}) {
@@ -20,7 +19,24 @@ export default function Posts({data}:{data:ForumPosts[]}) {
                   item.tags[param] !== undefined
                 )
               ).map((item, index)=>{
-                return <Card entry={item} key={item.title+index}/>
+                const date = item.datePosted.slice(3, -16) + item.datePosted.slice(0, 3);
+                const replies = item.replies > 0 ? `${item.replies} replies`: 'No replies';
+
+                return (
+                  <li className={styles['card-container']} key={item.title+index}>
+                    <a href={item.url} target='_' className={styles['card-wrapper']}>
+                      <div className={styles['card-content']}>
+                        <h2 className={styles['card-title']}>{item.title}</h2>
+                        <span className={styles['card-author']}>by {item.author}</span>
+                        <p>{item.content}</p>
+                      </div>
+                      <div className={styles['card-details']}>
+                        <span className={styles['card-date']}>{date}</span><br/>
+                        <span className={styles['card-replies']}>{replies}</span>
+                      </div>
+                    </a>
+                  </li>
+                )
               })
             }
           </ul>
