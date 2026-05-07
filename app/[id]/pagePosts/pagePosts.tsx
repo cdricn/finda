259592db366c02eb96f2747fcd358cb457a39929
cdricn <cdricncdricn@gmail.com>
2026-05-08 01:37:29 +0800

    Added fetching parent for suspense

---
+++ app/[id]/pagePosts/pagePosts.tsx
@@ -0,0 +1,16 @@
+import { fetchData } from '@/app/api/fetch/fetchData';
+import Posts from './posts';
+
+export default async function PagePosts({params}:{params:string}) {
+  
+  let data = await fetchData.getResource(params, 'gamejam/posts');
+
+  return (
+    <>
+      {data && Object.keys(data).length > 0 ?
+        <Posts data={data}/> :
+        <></>
+      }
+    </>
+  )
+}
