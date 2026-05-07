9fc19477bd091b47525e4584bf71a199269dd635
cdricn <cdricncdricn@gmail.com>
2026-05-08 01:27:49 +0800

    Fixing suspense

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,23 +1,23 @@
 import styles from './page.module.css';
 import FilterJams from '../filter/filterJams';
 import FilterTags from '../filter/filterTags';
-import Posts from '../posts/posts';
-import PageInfo from '../pageInfo/pageInfo';
+import Posts from './pagePosts/posts';
+import PageInfo from './pageInfo/pageInfo';
 import { fetchData } from '../api/fetch/fetchData';
 import { Suspense } from 'react';
 
 export default async function PostsPage({params}:{params: Promise<{ id: string }>}) {
   const paramID = await params;
 
-  let pageDetails = await fetchData.getResource(paramID.id, 'gamejam/details');
+  // put within
   let pagePostList = await fetchData.getResource(paramID.id, 'gamejam/posts');
 
   return (
     <>
       <main className={styles['main-container']}>
         <section className={styles['title-section']}>
-          <Suspense fallback={<></>}>
-            <PageInfo data={pageDetails} params={paramID.id}/>
+          <Suspense fallback={<>Loading</>}>
+            <PageInfo params={paramID.id}/>
           </Suspense>
           <Suspense fallback={<></>}>
             <FilterJams />
