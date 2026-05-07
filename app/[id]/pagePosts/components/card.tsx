151b73945bade8decd9bfb01dde3d325591a0eac
cdricn <cdricncdricn@gmail.com>
2026-05-08 01:27:31 +0800

    Relocated and renamed components

---
+++ app/[id]/pagePosts/components/card.tsx
@@ -0,0 +1,24 @@
+import styles from './card.module.css';
+import { ForumPosts } from '@/app/lib/interface';
+
+export default function Card({entry}:{entry:ForumPosts}) {
+
+  const date = entry.datePosted.slice(3, -16) + entry.datePosted.slice(0, 3);
+  const replies = entry.replies > 0 ? `${entry.replies} replies`: 'No replies';
+
+  return (
+    <li className={styles['card-container']}>
+      <a href={entry.url} target='_' className={styles['card-wrapper']}>
+        <div className={styles['card-content']}>
+          <h2 className={styles['card-title']}>{entry.title}</h2>
+          <span className={styles['card-author']}>by {entry.author}</span>
+          <p>{entry.content}</p>
+        </div>
+        <div className={styles['card-details']}>
+          <span className={styles['card-date']}>{date}</span><br/>
+          <span className={styles['card-replies']}>{replies}</span>
+        </div>
+      </a>
+    </li>
+  )
+}
---
+++ app/[id]/pagePosts/components/message.tsx
@@ -0,0 +1,16 @@
+import styles from './message.module.css';
+import { ParamValue } from 'next/dist/server/request/params';
+
+export default function Message({text, link}:{text:string, link:ParamValue}) {
+
+  const communityLink = link ? 'https://itch.io/jam/'+ link.toString() + '/community': '';
+
+  return (
+    <div className={styles['footer-container']}>
+      <span className={styles['footer-text']}>{text}</span>
+      <a href={communityLink} className={styles['button-container']} target='_'>
+        <span className={styles['button-text']}>Make a post</span>
+      </a>
+    </div>
+  )
+}
