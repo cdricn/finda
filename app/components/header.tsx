ed81a9bc1d445ab6e606c79215871a99a2dc88f9
cdricn <cdricncdricn@gmail.com>
2026-05-09 23:29:56 +0800

    Temporarily removed logo from header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,9 +5,6 @@ import { DiGithubBadge } from "react-icons/di";
 export default function Header() {
   return (
     <header className={styles['header-container']}>
-      <Link href={'https://findateam.vercel.app/'} className={styles['header-title']}>
-        findateam
-      </Link>
       <a href='https://github.com/cdricn/finda' target='_' 
         className={styles['github-icon']}>
           <DiGithubBadge />
