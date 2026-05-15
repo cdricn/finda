4e31ae0ac3ddab9e4ce57583f62f429a0ff09b85
cdricn <cdricncdricn@gmail.com>
2026-05-15 18:31:59 +0800

    Added fetching component for list of game jams

---
+++ app/(root)/gamejamList/gamejamList.tsx
@@ -0,0 +1,16 @@
+import { fetchData } from '@/app/api/fetch/fetchData';
+import Table from './components/table';
+
+export default async function GameJamList({params}:{params:string}) {
+  
+  let upcoming = await fetchData.getJams('gamejams/upcoming/minMembers', 300);
+  let ongoing = await fetchData.getJams('gamejams/ongoing/minMembers', 300);
+
+  let data = {ongoing: ongoing, upcoming: upcoming}
+
+  return (
+    <>
+      <Table data={data}/>
+    </>
+  )
+}
