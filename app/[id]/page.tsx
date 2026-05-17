8071a35f0d9659faec1e82d98a94aa0e0818c46c
cdricn <cdricncdricn@gmail.com>
2026-05-18 00:03:33 +0800

    Changed a tag to nextjs Link

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,16 +1,17 @@
 import styles from './page.module.css';
 import FilterTags from '../filter/filterTags';
 import PageInfo from './pageInfo/pageInfo';
-import { Suspense } from 'react';
 import PagePosts from './pagePosts/pagePosts';
+import { Suspense } from 'react';
+import Link from 'next/link';
 
 export default async function PostsPage({params}:{params: Promise<{ id: string }>}) {
   const paramID = await params;
 
   return (
     <main className={styles['main-container']}>
-      <a href='/' className={styles['back-button']}></a>
       <div className={styles['wrapper']}>
+        <Link href='/' className={styles['back-button']}></Link>
         <section className={styles['title-section']}>
           <Suspense fallback={<>Loading</>}>
             <PageInfo params={paramID.id}/>
