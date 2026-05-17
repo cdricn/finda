597d2d5660098ef34b9d477c0e729b85455402ea
cdricn <cdricncdricn@gmail.com>
2026-05-17 17:55:25 +0800

    Added error handling for failed fetches

--- app/api/fetch/fetchData.ts
+++ app/api/fetch/fetchData.ts
@@ -1,8 +1,20 @@
 const apiLink = process.env.API_ENDPOINT;
 
 async function FetchApi(path:string) {
-  const data = await fetch(`${apiLink}/${path}`);
-  return data.json();
+  try {
+    const response = await fetch(`${apiLink}/${path}`, { method: "GET"});
+
+    if (response.status === 404) {
+      throw new Error('An error occured while fetching the data.');
+    }
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+  }
+  catch {
+    return null
+  }
 } 
 
 export const fetchData = {
