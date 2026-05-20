e6bc275228aa12118525c68e160781257fb1d11c
cdricn <cdricncdricn@gmail.com>
2026-05-21 00:21:43 +0800

    Removed header container to just the icon

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,11 +3,9 @@ import { DiGithubBadge } from "react-icons/di";
 
 export default function Header() {
   return (
-    <header className={styles['header-container']}>
-      <a href='https://github.com/cdricn/finda' target='_' 
-        className={styles['github-icon']}>
-          <DiGithubBadge />
-      </a>
-    </header>
+    <a href='https://github.com/cdricn/finda' target='_' 
+      className={styles['github-icon']}>
+        <DiGithubBadge />
+    </a>
   )
 }
